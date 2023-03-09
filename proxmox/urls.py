b4a53bb7572e2798d8f2aad61df450acc38a2d8b
nitsuh21 <nitsuhdemissew21@gmail.com>
2023-03-09 16:30:08 +0300

    third commit

--- proxmox/urls.py
+++ proxmox/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('zcldadmins/admin/', admin.site.urls),
     path('', include("home.urls")),
+    path('api/', include("api.urls")),
 ]
 
 if settings.DEBUG:
